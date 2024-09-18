6f6d13457fb4fd17494c6b49cec898393a9e85d5
Nicolas <nicolasgfalabella@gmail.com>
2024-09-18 07:17:43 -0300

    Tamaño de interfaz

--- src/Formulario.java
+++ src/Formulario.java
@@ -781,56 +781,120 @@
 //     }
 // }
 
+// import javax.swing.*;
+// import java.awt.* ;
+// import java.awt.event.*;
+
+// public class Formulario extends JFrame implements ActionListener{
+
+//     private JMenuBar menubar ;
+//     private JMenu menu1 ;
+//     private JMenuItem menuItem1, menuItem2, menuItem3 ;
+
+//     public Formulario(){
+//         setLayout(null);
+//         menubar = new JMenuBar() ;
+//         setJMenuBar(menubar);
+
+//         menu1 = new JMenu("Opciones") ;
+//         menubar.add(menu1) ;
+
+//         menuItem1 = new JMenuItem("Rojo") ;
+//         menuItem1.addActionListener(this) ;
+//         menu1.add(menuItem1) ;
+
+        
+//         menuItem2 = new JMenuItem("Verde") ;
+//         menuItem2.addActionListener(this) ;
+//         menu1.add(menuItem2) ;
+
+//         menuItem3 = new JMenuItem("Azul") ;
+//         menuItem3.addActionListener(this) ;
+//         menu1.add(menuItem3) ;
+//     }
+
+//     public void actionPerformed(ActionEvent e) {
+//         Container fondo = this.getContentPane() ;
+//         if(e.getSource() == menuItem1){
+//             fondo.setBackground(new Color(255,0,0));
+//         }
+//         if(e.getSource() == menuItem2) {
+//             fondo.setBackground(new Color(0,255,0));
+//         }
+//         if(e.getSource() == menuItem3) {
+//             fondo.setBackground(new Color(0,0,255));
+//         }
+//     }
+
+//     public static void main ( String args[]) {
+//         Formulario formulario = new Formulario() ;
+//         formulario.setBounds(0, 0, 400, 300);
+//         formulario.setVisible(true);
+//         formulario.setLocationRelativeTo(null);
+//     }
+  
+// }
+
 import javax.swing.*;
 import java.awt.* ;
 import java.awt.event.*;
 
 public class Formulario extends JFrame implements ActionListener{
-
     private JMenuBar menubar ;
-    private JMenu menu1 ;
-    private JMenuItem menuItem1, menuItem2, menuItem3 ;
+    private JMenu menu1,menu2,menu3 ;
+    private JMenuItem menuItem1 , menuItem2, menuItem3 , menuItem4 ;
 
-    public Formulario(){
-        setLayout(null);
+    public Formulario() {
+        setLayout(null) ;
         menubar = new JMenuBar() ;
         setJMenuBar(menubar);
 
         menu1 = new JMenu("Opciones") ;
-        menubar.add(menu1) ;
+        menubar.add(menu1);
 
-        menuItem1 = new JMenuItem("Rojo") ;
-        menuItem1.addActionListener(this) ;
-        menu1.add(menuItem1) ;
+        menu2 = new JMenu("Tamaño de la ventana") ;
+        menu1.add(menu2) ;
 
-        
-        menuItem2 = new JMenuItem("Verde") ;
-        menuItem2.addActionListener(this) ;
-        menu1.add(menuItem2) ;
+        menu3 = new JMenu("Color de fondo") ;
+        menu1.add(menu3) ;
 
-        menuItem3 = new JMenuItem("Azul") ;
-        menuItem3.addActionListener(this) ;
-        menu1.add(menuItem3) ;
+        menuItem1 = new JMenuItem("320x480") ;
+        menu2.add(menuItem1) ;
+        menuItem1.addActionListener(this);
+
+        menuItem2 = new JMenuItem("480x640") ;
+        menu2.add(menuItem2) ;
+        menuItem2.addActionListener(this);
+
+        menuItem3 = new JMenuItem("Rojo") ;
+        menu3.add(menuItem3) ;
+        menuItem3.addActionListener(this);
+
+        menuItem4 = new JMenuItem("Verde") ;
+        menu3.add(menuItem4) ;
+        menuItem4.addActionListener(this);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        Container fondo = this.getContentPane() ;
-        if(e.getSource() == menuItem1){
-            fondo.setBackground(new Color(255,0,0));
+    public void actionPerformed(ActionEvent e ) {
+        if( e.getSource() == menuItem1) {
+            setSize(320 , 480);
         }
-        if(e.getSource() == menuItem2) {
-            fondo.setBackground(new Color(0,255,0));
+        if( e.getSource() == menuItem2) {
+            setSize(480 , 640);
         }
-        if(e.getSource() == menuItem3) {
-            fondo.setBackground(new Color(0,0,255));
+        if( e.getSource() == menuItem3) {
+            getContentPane().setBackground( new Color(255,0,0) );
+        }
+        if( e.getSource() == menuItem4) {
+            getContentPane().setBackground( new Color(0,255,0) );
         }
     }
 
-    public static void main ( String args[]) {
+    public static void main(String args[]) {
         Formulario formulario = new Formulario() ;
-        formulario.setBounds(0, 0, 400, 300);
+        formulario.setBounds(0,0,300,300);
         formulario.setVisible(true);
+        formulario.setResizable(false);
         formulario.setLocationRelativeTo(null);
     }
-  
 }
